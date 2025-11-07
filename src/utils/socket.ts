9a49bae7e9b87d467ad2f44d42a71ad3acd4314b
Akash raikwar <akashraikwar.mongoose@gmail.com>
2025-11-07 19:20:45 +0530

    dsfsdfsdaf

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,5 +1,11 @@
+// import { io, Socket } from 'socket.io-client';
+
+// const socket: Socket = io('https://akash-hrms-backend.onrender.com');
+
+// export default socket;
+
 import { io, Socket } from 'socket.io-client';
 
-const socket: Socket = io('http://localhost:5000');
+const socket: Socket = io((import.meta as any).env.VITE_API_URL?.replace('/api', '') || 'http://localhost:5000');
 
 export default socket;
